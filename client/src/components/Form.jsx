b5ed3ca0a164c8cc7f5eea8eaa52d4cf2e8d531c
saloni kumawat <545saloni@gmail.com>
2023-10-26 12:45:48 +0200

    Set useState hook in Form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 import "../index.css";
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function submitItems(event) {
+    event.preventDefault();
+
+    
+  }
   return (
     <div>
-      <form className="add-form">
+      <form className="add-form" onSubmit={submitItems}>
         <h3>What do you need for your 😍 trip?</h3>
-        <select>
-          {Array.from({ length: 20 }, (_,current) => current + 1).map
-          (
-            (num) => (<option value={num} key={num}>{num}</option>)
+        <select
+          value={quantity}
+          onChange={(event) => setQuantity(Number(event.target.value))}
+        >
+          {Array.from({ length: 20 }, (_, current) => current + 1).map(
+            (num) => (
+              <option value={num} key={num}>
+                {num}
+              </option>
+            )
           )}
-          
         </select>
-        <input type="text" placeholder="Item...."></input>
-        <button>Add</button>
+        <input
+          type="text"
+          placeholder="Item...."
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        ></input>
+        <button type="submit">Add</button>
       </form>
     </div>
   );
